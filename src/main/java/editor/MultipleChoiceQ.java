d06d3e18bb4ddf9832f91356329b617677867481
Milana Dundic <milana.dundic@namics.com>
2018-06-14 21:44:07 +0200

    Save file

--- src/main/java/editor/MultipleChoiceQ.java
+++ src/main/java/editor/MultipleChoiceQ.java
@@ -3,8 +3,13 @@ package main.java.editor;
 import main.java.editor.MultipleChoiceA;
 import main.java.editor.Question;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MultipleChoiceQ extends Question {
@@ -16,7 +21,7 @@ public class MultipleChoiceQ extends Question {
         for (int i = 0; i < answersJson.length(); i++) {
             JSONObject oneAnswer = answersJson.optJSONObject(i);
             answers.add(new MultipleChoiceA(
-                oneAnswer.optString("answerText", "error"),
+                oneAnswer.optString("text", "error"),
                 oneAnswer.optBoolean("correct", false)
             ));
         }
@@ -28,6 +33,24 @@ public class MultipleChoiceQ extends Question {
 
     @Override
     public JSONObject save() {
-        return null;
+        try {
+            JSONObject jsonObject = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            answers.forEach(a->{
+                try {
+                    jsonArray.put(new JSONObject().put("text", a.getText()).put("correct", a.isCorrect()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+            jsonObject.put("questionText", getQuestionText())
+                    .put("timer", getTimer())
+                    .put("type", getType());
+            jsonObject.put("answers",jsonArray);
+            return jsonObject;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
